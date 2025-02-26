ff5a11c756224816ce03c585b6c0551909b70376
Perpetual Akinyi <you@example.com>
2025-02-26 10:24:45 +0300

    made some changes to the navbar and made a course page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Homepage from './pages/Homepage';
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Navbar from './components/Navbar';
+import CoursesPage from './pages/Coursepage';
+import StudentDashboard from './Dashboards/StudentDashboard';
+import TeacherDashboard from './Dashboards/TeacherDashboard';
+import AdminDashboard from './Dashboards/AdminDashboard';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
@@ -17,6 +21,10 @@ function App() {
         <Route path="/" element={<Homepage loggedIn={loggedIn}/>} />
         <Route path="/signup" element={<SignUp setLoggedIn={setLoggedIn}/>} />
         <Route path="/login" element={<Login setLoggedIn={setLoggedIn}/>} />
+        <Route path="/courses" element={<CoursesPage/>} />
+        <Route path="/dashboard/student" element={<StudentDashboard />} />
+        <Route path="/dashboard/teacher" element={<TeacherDashboard />} />
+        <Route path="/dashboard/admin" element={<AdminDashboard />} />
       </Routes>
     </Router>
  
