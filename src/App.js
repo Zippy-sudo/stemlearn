301e4f46c3162c32847c7a341771fe04831e88dd
Zippy-sudo <nehemiahmadahana230@gmail.com>
2025-03-05 12:41:10 +0300

    Update App.js
    Modifications

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -11,27 +10,30 @@ import AdmDashboard from './pages/AdmDashboard';
 import AdminRoute from './components/protectedRoute';
 import CoursesPage from './pages/Coursepage';
 import StudentDashboard from './pages/StudentDashboard';
+import MyCourses from './components/MyCourses';
+import Enrollment from './components/Enrollment';
 import TeacherRoute from './components/TeacherRoute';
 import TeacherDashboard from './pages/TeacherDashboard';
 
 function App() {
-  const baseURL = "https://stemlearn-app-db.onrender.com"
+  const baseURL = "https://superb-duckanoo-18547b.netlify.app"
   const [loggedIn, setLoggedIn] = useState(false)
 
   return (
     <Router>
-      <Navbar />
+      <Navbar loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
       <Routes>
         <Route path="/" element={<Homepage loggedIn={loggedIn} baseURL={baseURL}/>} />
-        <Route path="/signup" element={<SignUp setLoggedIn={setLoggedIn} baseURL={baseURL}/>} />
-        <Route path="/login" element={<Login setLoggedIn={setLoggedIn} baseURL={baseURL}/>} />
+        <Route path="/Signup" element={<SignUp setLoggedIn={setLoggedIn} baseURL={baseURL}/>} />
+        <Route path="/Login" element={<Login setLoggedIn={setLoggedIn} baseURL={baseURL}/>} />
         <Route path="/AdmDashboard" element={<AdmDashboard baseURL={baseURL}/>} />
-        <Route path="/admin/dashboard" element={<AdminRoute><AdmDashboard baseURL={baseURL}/></AdminRoute>} />
+        <Route path="/admin/dashboard" element={<AdmDashboard baseURL={baseURL}/>} />
         <Route path="/TeacherDashboard" element={<TeacherDashboard baseURL={baseURL}/>} />
-        <Route path="/teacher/dashboard" element={<TeacherRoute><TeacherDashboard baseURL={baseURL}/></TeacherRoute>} />
-        <Route path="/courses" element={<CoursesPage/>}/>
-        <Route path="/studentDashboard" element={<StudentDashboard/>}/>
-
+        <Route path="/teacher/dashboard" element={<TeacherDashboard baseURL={baseURL}/>} />
+        <Route path="/Courses" element={<CoursesPage baseURL={baseURL} loggedIn={loggedIn}/>}/>
+        <Route path="/StudentDashboard" element={<StudentDashboard baseURL={baseURL}/>}/>
+        <Route path="/Mycourses" element ={<MyCourses/>}/>
+        <Route path="/Enroll/:courseId" element={<Enrollment baseURL={baseURL}/>}/>
       </Routes>
     </Router>
  
@@ -39,3 +41,4 @@ function App() {
   )}
 
 export default App;
+
