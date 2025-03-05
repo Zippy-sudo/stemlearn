6aa2100f7d68f9cb950098ad9d18383c07d596eb
Oliver cheruiyot <olivercher1000@gmail.com>
2025-03-05 15:24:36 +0300

    Add StudentQuizDashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,71 @@
-
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from './pages/Homepage';
+import Homepage from "./pages/Homepage";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
-import AdmDashboard from './pages/AdmDashboard';
-import AdminRoute from './components/protectedRoute';
-import CoursesPage from './pages/Coursepage';
-import StudentDashboard from './pages/StudentDashboard';
-import MyCourses from './components/MyCourses';
-import Enrollment from './components/Enrollment';
+import AdmDashboard from "./pages/AdmDashboard";
+import AdminRoute from "./components/protectedRoute";
+import CoursesPage from "./pages/Coursepage";
+import StudentDashboard from "./pages/StudentDashboard";
+import MyCourses from "./components/MyCourses";
+import Enrollment from "./components/Enrollment";
+import StudentQuizDashboard from "./pages/StudentQuizDashboard";
 
 function App() {
-  const baseURL = "https://stemlearn-app-db.onrender.com"
-  const [loggedIn, setLoggedIn] = useState(false)
+  const baseURL = "http://127.0.0.1:5555";
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <Router>
-      <Navbar loggedIn={loggedIn}/>
+      <Navbar loggedIn={loggedIn} />
       <Routes>
-        <Route path="/" element={<Homepage loggedIn={loggedIn} baseURL={baseURL}/>} />
-        <Route path="/Signup" element={<SignUp setLoggedIn={setLoggedIn} baseURL={baseURL}/>} />
-        <Route path="/Login" element={<Login setLoggedIn={setLoggedIn} baseURL={baseURL}/>} />
-        <Route path="/AdmDashboard" element={<AdmDashboard baseURL={baseURL}/>} />
-        <Route path="/admin/dashboard" element={<AdminRoute><AdmDashboard baseURL={baseURL}/></AdminRoute>} />
-        <Route path="/Courses" element={<CoursesPage baseURL={baseURL} loggedIn={loggedIn}/>}/>
-        <Route path="/StudentDashboard" element={<StudentDashboard baseURL={baseURL}/>}/>
-        <Route path="/Mycourses" element ={<MyCourses/>}/>
-        <Route path="/Enroll/:courseId" element={<Enrollment baseURL={baseURL}/>}/>
-
+        <Route
+          path="/"
+          element={<Homepage loggedIn={loggedIn} baseURL={baseURL} />}
+        />
+        <Route
+          path="/Signup"
+          element={<SignUp setLoggedIn={setLoggedIn} baseURL={baseURL} />}
+        />
+        <Route
+          path="/Login"
+          element={<Login setLoggedIn={setLoggedIn} baseURL={baseURL} />}
+        />
+        <Route
+          path="/AdmDashboard"
+          element={<AdmDashboard baseURL={baseURL} />}
+        />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <AdminRoute>
+              <AdmDashboard baseURL={baseURL} />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/Courses"
+          element={<CoursesPage baseURL={baseURL} loggedIn={loggedIn} />}
+        />
+        <Route
+          path="/StudentDashboard"
+          element={<StudentDashboard baseURL={baseURL} />}
+        />
+        <Route path="/Mycourses" element={<MyCourses />} />
+        <Route
+          path="/Enroll/:courseId"
+          element={<Enrollment baseURL={baseURL} />}
+        />
+        <Route
+          path="/teacher-quizpage"
+          element={<StudentQuizDashboard baseURL={baseURL} />}
+        />
       </Routes>
     </Router>
- 
-    
-  )}
+  );
+}
 
 export default App;
