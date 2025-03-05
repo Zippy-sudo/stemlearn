9c3bc01059d7c708e4a10213f80279445bd2b592
BlessingOkora <moraablessing082@gmail.com>
2025-03-05 16:11:56 +0300

    Adds teacher/admin features

---
+++ src/components/TeacherRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+
+function TeacherRoute({ children }) {
+  const role = localStorage.getItem("role");
+
+  if (role !== "TEACHER") {
+    alert("Unauthorized access!");
+    return <Navigate to="/" />;
+  }
+  return children;
+}
+
+export default TeacherRoute;
