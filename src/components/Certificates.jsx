a5e95209c009efacefd2f549114d4b7318968a9f
Perpetual Akinyi <you@example.com>
2025-03-06 16:41:21 +0300

    Added a LessonsPage

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 function Certificates () {
+  //const [certificates, setCertificates] = useState([]);
+  //const [loading, setLoading] = useState(true);
+
+  //useEffect (() => {
+   // const fetchCertificates = async () => {
+    //  try {
+      //  const response = await fetch(`http://127.0.0.1:5555/certificates/${studentId}`);
+      //}
+  //}})
+
+  
 
   return (<div>
     <h1>Certificates</h1>
