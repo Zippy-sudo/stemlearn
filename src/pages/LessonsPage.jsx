564a7f8c99c108d149f6cb350565a66236c25035
Perpetual Akinyi <you@example.com>
2025-03-06 16:44:48 +0300

    Added a fetch to lessons

--- src/pages/LessonsPage.jsx
+++ src/pages/LessonsPage.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function LessonsPage (){
-    return ()
+    const [lessons, setLessons] = useState([]);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:5555/lessons", {
+            method: "GET",
+            headers: {
+              "Authorization": `Bearer ${sessionStorage.getItem("Token")}`,
+              "Content-Type": "application/json"
+            }
+          })
+          .then(response => response.json())
+          .then(data => {
+            console.log("API Response:", data);  // Check the actual response
+            setLessons(Array.isArray(data) ? data : []); // Ensure it's an array
+          })
+          .catch(error => console.error("Error fetching lessons:", error));
+        }, []);
+        
+    
+
 }
 
 export default LessonsPage
