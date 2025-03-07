f7c3f2ff83a87a3da142f9743dd4d738a1771a4d
Oliver cheruiyot <olivercher1000@gmail.com>
2025-03-07 15:37:34 +0300

    update studentquizpage css import

--- src/pages/StudentQuizDashboard.jsx
+++ src/pages/StudentQuizDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "../StudentQuizDashboard.css";
 
 const StudentQuizDashboard = ({ baseURL }) => {
   const [quizzes, setQuizzes] = useState([]);
