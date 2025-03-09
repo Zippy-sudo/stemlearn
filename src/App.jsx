0a62cfa2295ad4dde6bacea9ef1864514e72aa8c
Zippy-sudo <nehemiahmadahana230@gmail.com>
2025-03-09 20:35:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
